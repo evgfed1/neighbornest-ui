69bb41a137b30607847f72a886961e3f47d65e31
anton kovaltshuk <kovaljski@gmail.com>
2024-01-25 13:12:31 +0200

    wip

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -14,27 +14,28 @@
         </li>
       </ul>
 
-      <button @click="toggleForm" class="btn btn-outline-dark">Add news</button>
+      <button v-if="statusAdmin" @click="toggleForm" class="btn btn-outline-dark">Add news</button>
 
       <div v-if="showForm">
         <h2>Create news</h2>
         <form @submit.prevent="addNews">
           <div class="mb-3">
             <label for="titleInput" class="form-label">Title:</label>
-            <input type="text" id="titleInput" class="form-control" v-model="newNewsTitle" required>
+            <input type="text" id="titleInput" class="form-control" v-model="newsRequest.title" required>
           </div>
 
           <div class="mb-3">
-            <label for="dateTimeInput" class="form-label">Date/time:</label>
-            <input type="datetime-local" id="dateTimeInput" class="form-control" v-model="newNewsDateTime" required>
+            <label for="dateTimeInput" class="form-label">Event date/time:</label>
+            <!--                        <vue-flatpickr v-model="newNewsDateTime" type="text"></vue-flatpickr>-->
+            <input type="datetime-local" id="dateTimeInput" class="form-control" v-model="newsRequest.eventDate" required>
           </div>
 
           <div class="mb-3">
             <label for="newsInput" class="form-label">Content:</label>
-            <textarea id="newsInput" class="form-control" rows="5" v-model="newNewsText" required></textarea>
+            <textarea id="newsInput" class="form-control" rows="5" v-model="newsRequest.content" required></textarea>
           </div>
 
-          <button type="submit" class="btn btn-outline-dark">Add</button>
+          <button @click="sendNewNewsRequest" type="submit" class="btn btn-outline-dark">Add</button>
         </form>
       </div>
 
@@ -44,44 +45,70 @@
 </template>
 
 <script>
+
+
+import VueFlatpickr from "vue-flatpickr-component";
+
+
 export default {
   name: "NewsView",
+  components: {VueFlatpickr},
+
   data() {
     return {
+      statusAdmin: false,
+      roleName: sessionStorage.getItem('roleName'),
       newsList: [],
       showForm: false,
-      newNewsText: "",
-      newNewsTitle: "",
-      newNewsDateTime: "",
+      newsRequest: {
+        userId: sessionStorage.getItem('userId'),
+        content: "",
+        title: "",
+        eventDate: "",
+      }
     };
   },
   methods: {
 
-n
+    sendNewNewsRequest() {
+      this.$http.post("/news/create", this.newsRequest
+      ).then(response => {
+
+      }).catch(error => {
+
+      });
+    },
+
+    checkAdminStatus() {
+      if (this.roleName === 'admin') {
+        this.statusAdmin = !this.statusAdmin;
+      }
+    },
+
     toggleForm() {
       this.showForm = !this.showForm;
     },
 
     addNews() {
-      if (this.newNewsText.trim() !== "" &&
-          this.newNewsTitle.trim() !== "" &&
-          this.newNewsDateTime.trim() !== "") {
+      if (this.newsRequest.content.trim() !== "" &&
+          this.newsRequest.title.trim() !== "" &&
+          this.newsRequest.eventDate.trim() !== "") {
         const newNews = {
-          title: this.newNewsTitle,
-          dateTime: this.newNewsDateTime,
-          text: this.newNewsText,
+          title: this.newsRequest.title,
+          dateTime: this.newsRequest.eventDate,
+          text: this.newsRequest.content,
         };
         this.newsList.push(newNews);
-        this.newNewsTitle = "";
-        this.newNewsDateTime = "";
-        this.newNewsText = "";
+        this.newsRequest.title = "";
+        this.newsRequest.eventDate = "";
+        this.newsRequest.content = "";
         this.showForm = false;
       }
     },
   },
+  mounted() {
+    this.checkAdminStatus()
+  }
 
 }
 </script>
-
-
-
